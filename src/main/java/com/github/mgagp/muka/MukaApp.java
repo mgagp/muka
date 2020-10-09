09cfc9c35723a65dc7e6186d46cefd58a3d6faed
Marc Gagnon <marcgagnonpincourt@gmail.com>
2020-10-09 02:32:34 +0000

    Initial commit

---
+++ src/main/java/com/github/mgagp/muka/MukaApp.java
@@ -0,0 +1,20 @@
+package com.github.mgagp.muka;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MukaApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MukaApp.class, args);
+	}
+
+    @Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return null;
+    }
+}
